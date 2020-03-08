b00154a6201fb3cc7b07ff28bff856267b362757
Willy Metcalf <william.g.metcalf@gmail.com>
2020-03-07 21:39:53 -0600

    so we can see stuff

--- src/App.js
+++ src/App.js
@@ -1,26 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import ApplicationViews from "./ApplicationViews"
+import NavBar from "./nav/NavBar"
+import Login from "./auth/Login"
+import Register from "./auth/Register"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
-export default App;
+export default () => (
+  <>
+    <Route render={props => <NavBar {...props} />} />
+    <Route render={props => <ApplicationViews {...props} />} />
+
+  </>
+)
+
+
